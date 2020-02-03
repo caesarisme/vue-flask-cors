766401dd3e73bb4124924ce8589613d96ded59c9
Kaisar Kuanysh <kaisar@MacBook-Pro-Kaisar.local>
2020-02-03 15:11:24 +0600

    initial commit

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+# Config
+DEBUG = True
+
+
+# Init app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Enable CORS
+CORS(app)
+
+# Fake data
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    },
+]
+
+# Routes
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {'status': 'success'}
+
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+
+    return jsonify(response_object)
+
+
+if __name__ == "__main__":
+    app.run()
